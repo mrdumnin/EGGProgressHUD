ffb9dbb7db3d5e0ba65421dcff1a5a9a40d91474
Suchon Dumnin <mrdumnin@gmail.com>
2016-10-07 12:18:01 +0700

    update setting

---
+++ EGGProgressHUD/Classes/EGGProgressHUD.swift
@@ -0,0 +1,15 @@
+//
+//  EGGProgressHUD.swift
+//  Pods
+//
+//  Created by Suchon Dumnin on 10/7/2559 BE.
+//
+//
+
+import UIKit
+
+public class EGGProgressHUD: NSObject {
+    func show() {
+        print("abc 1234")
+    }
+}
---
+++ Example/EGGProgressHUD_Tests/EGGProgressHUD_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  EGGProgressHUD_Tests.swift
+//  EGGProgressHUD_Tests
+//
+//  Created by Suchon Dumnin on 10/7/2559 BE.
+//  Copyright © 2559 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class EGGProgressHUD_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+
+    }
+    
+}
