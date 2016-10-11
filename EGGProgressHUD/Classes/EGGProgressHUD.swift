12a6cb277276542ae2435283e7222cd341e90505
Suchon Dumnin <mrdumnin@gmail.com>
2016-10-11 10:56:36 +0700

    Progress loading

--- EGGProgressHUD/Classes/EGGProgressHUD.swift
+++ EGGProgressHUD/Classes/EGGProgressHUD.swift
@@ -12,15 +12,19 @@ public class EGGProgressHUD: NSObject {
     
     private var spinner: UIActivityIndicatorView!
     private var loadingBGView: UIView!
+    private var progressView: UIProgressView!
+    private var loadingLabel: UILabel!
     
     //MARK: - Property user can set
     public var type: ProgressType!
     public var style: SpinnerStyle!
     public var bgColor: UIColor!
+    //public var progress: Float!
     
     public enum ProgressType {
         case ProgressWithoutBG
         case ProgressWithBG
+        case ProgressView
     }
     
     public enum SpinnerStyle {
@@ -35,34 +39,48 @@ public class EGGProgressHUD: NSObject {
         self.type = ProgressType.ProgressWithoutBG
         self.style = SpinnerStyle.Gray
         self.bgColor = UIColor.blackColor()
+        //self.progress = 0.0
     }
     
     public func showInView(view: UIView) {
         
-        if self.type == ProgressType.ProgressWithBG {
-            
-            self.setupProgressWithoutBG()
-            self.setupProgressWithBG()
-            
-            self.loadingBGView.hidden = false
-            self.loadingBGView.removeFromSuperview()
-            view.addSubview(self.loadingBGView)
-        } else if self.type == ProgressType.ProgressWithoutBG {
-            
+         if self.type == ProgressType.ProgressWithoutBG {
             self.setupProgressWithoutBG()
             
             self.spinner.removeFromSuperview()
             view.addSubview(self.spinner)
             
             self.spinner.startAnimating()
+         } else if self.type == ProgressType.ProgressWithBG {
+            self.setupProgressWithoutBG()
+            self.setupProgressWithBG()
+            
+            self.loadingBGView.hidden = false
+            self.loadingBGView.removeFromSuperview()
+            view.addSubview(self.loadingBGView)
+         } else if self.type == ProgressType.ProgressView {
+            self.setupProgressView()
+            
+            self.loadingBGView.hidden = false
+            self.loadingBGView.removeFromSuperview()
+            view.addSubview(self.loadingBGView)
         }
     }
     
     public func hide() {
-        if self.type == ProgressType.ProgressWithBG {
+        if self.type == ProgressType.ProgressWithBG || self.type == ProgressType.ProgressView {
             self.loadingBGView.hidden = true
         } else if self.type == ProgressType.ProgressWithoutBG {
             self.spinner.stopAnimating()
+        }
+    }
+    
+    public func setProgress(progress: Float) {
+        guard (self.progressView == nil) else {
+            dispatch_async(dispatch_get_main_queue()) {
+                self.progressView.setProgress(progress, animated: true)
+            }
+            return
         }
     }
     
@@ -105,5 +123,38 @@ public class EGGProgressHUD: NSObject {
             , spinnerSize.width, spinnerSize.height)
         self.loadingBGView.addSubview(self.spinner)
         self.spinner.startAnimating()
+    }
+    
+    private func setupProgressView() {
+        self.loadingBGView = UIView()
+        let screenSize = UIScreen.mainScreen().bounds.size
+        let bgSize = CGSize(width: 200, height: 60)
+        let x = (screenSize.width / 2) - (bgSize.width / 2)
+        let y = (screenSize.height / 2) - (bgSize.height / 2)
+        self.loadingBGView.frame = CGRectMake(x, y, bgSize.width, bgSize.height)
+        self.loadingBGView.backgroundColor = self.bgColor
+        self.loadingBGView.layer.cornerRadius = 5.0
+        self.loadingBGView.layer.borderColor = UIColor.grayColor().CGColor
+        self.loadingBGView.layer.borderWidth = 0.5
+        self.loadingBGView.clipsToBounds = true
+        
+        let padding: CGFloat = 10
+        let subViewWidth: CGFloat = bgSize.width - (padding * 2)
+        //Progress View
+        self.progressView = UIProgressView()
+        self.progressView.progressViewStyle = .Bar
+        self.progressView.frame = CGRectMake(padding, 20, subViewWidth, 21)
+        self.progressView.progress = 0.0
+        self.loadingBGView.addSubview(self.progressView)
+        
+        //Loading label
+        self.loadingLabel = UILabel()
+        self.loadingLabel.textAlignment = .Center
+        self.loadingLabel.frame = CGRectMake(padding, bgSize.height - 30, subViewWidth, 21)
+        self.loadingLabel.text = "Loading..."
+        self.loadingLabel.textColor = UIColor.blackColor()
+        
+        self.loadingBGView.addSubview(self.loadingLabel)
+        
     }
 }
--- Example/EGGProgressHUD/Demo3ViewController.swift
+++ Example/EGGProgressHUD/Demo3ViewController.swift
@@ -7,15 +7,45 @@
 //
 
 import UIKit
+import EGGProgressHUD
 
 class Demo3ViewController: UIViewController {
 
+    private var i = 0.0
+    private var timer: NSTimer!
+    private var a: EGGProgressHUD!
+    override func viewDidDisappear(animated: Bool) {
+        self.timer.invalidate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.a = EGGProgressHUD()
+        self.a.type = EGGProgressHUD.ProgressType.ProgressView
+        self.a.bgColor = UIColor.groupTableViewBackgroundColor()
+        
+        self.a.showInView(self.view)
+        
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(
+            1.0, target: self, selector: #selector(self.updateProgress),
+            userInfo: nil, repeats: true)
+        self.timer.fire()
     }
 
+    func updateProgress() {
+        
+        i = i + 0.1
+        print(i)
+        if i > 1.0 {
+            self.a.setProgress(Float(self.i))
+            i = 0.1
+        } else {
+            self.a.setProgress(Float(self.i))
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
